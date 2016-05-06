60abc696e5772601ee11da922533dd5fd4d99a98
mariussterling <marius.sterling@hu-berlin.de>
2016-05-06 11:10:27 +0200

    change of computing the acf-, skewness-, kurtosis-values etc.
    the for loop was exchanged for apply

--- SFEReturns/SFEReturns.R
+++ SFEReturns/SFEReturns.R
@@ -24,35 +24,20 @@ statistic for german and british blue chips, 2004 - 2014"
 print(msg1)
 
 
+# computing acf of order 1, skewness, kurtosis and bera jarque statistic
+acf_fun = function(tmp) {
+  return(cor(tmp[-1], tmp[-length(tmp)]))
 }
-
-x = as.matrix(x)
-result = matrix(1, ncol = 7, nrow = ncol(x), dimnames = list(st, c("rho(ret):", 
-    "rho n(ret^2):", "rho(|ret|):", "S:", "K:", "JB:", "JB p-value:")))
-	
-for (i in 1:ncol(x)) {
-    # start calculation
-    ret1 = log(x[2:n, i]) - log(x[1:(n - 1), i])
-    skew = skewness(ret1)
-    kurt = kurtosis(ret1)
-    ret1 = as.matrix(ret1)
-    ret2 = matrix(c(ret1^2))
-    ret3 = matrix(c(abs(ret1)))
-    n = nrow(ret1)
-    rho1 = cor(ret1[2:n], ret1[1:(n - 1)])
-    rho2 = cor(ret2[2:n], ret2[1:(n - 1)])
-    rho3 = cor(ret3[2:n], ret3[1:(n - 1)])
-    jb = jarque.bera.test(ret1)
-    
-    # end calculation
-    result[i, 1] = rho1
-    result[i, 2] = rho2
-    result[i, 3] = rho3
-    result[i, 4] = skew[1]
-    result[i, 5] = kurt[1]
-    result[i, 6] = jb[[1]]
-    result[i, 7] = jb[[3]]
+acf_ret = apply(X = ret, MARGIN = 2, FUN = acf_fun)
+acf_squ = apply(X = ret_squ, MARGIN = 2, FUN = acf_fun)
+acf_abs = apply(X = ret_abs, MARGIN = 2, FUN = acf_fun)
+skew    = apply(X = ret, MARGIN = 2, FUN = skewness)
+kurt    = apply(X = ret, MARGIN = 2, FUN = kurtosis)
+jb_test = function(tmp) {
+  jb = jarque.bera.test(tmp)
+  return(jb$statistic)
 }
+jb      = apply(X = ret, MARGIN = 2, FUN = jb_test)
 
 # Output
 msg2 = "first order auto correlation of returns, squared returns and absolute returns and skewness, kurtosis and Bera Jarque statistic for german blue chips, 1974 - 1996"
