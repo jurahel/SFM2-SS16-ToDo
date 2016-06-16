64f0ccf0d5ea6d71c04d7703cc756d83824b618d
sburgard <sophie.burgard@gmx.net>
2016-06-16 10:01:39 +0200

    updated code and plots, updated metainfos

--- SFEplotma1/SFEplotma1.R
+++ SFEplotma1/SFEplotma1.R
@@ -11,33 +11,15 @@ lapply(libraries, function(x) if (!(x %in% installed.packages())) {
 lapply(libraries, library, quietly = TRUE, character.only = TRUE)
 
 # parameter settings
-n1   = "10"
-n2   = "20"
-beta = "0.5"
-
-# Input n1
-message = "Please specify n1"
-default = n1
-n1 = winDialogString(message, default)
-n1 = type.convert(n1, na.strings = "NA", as.is = FALSE)
-
-# Input n2
-message = "Please specify n2"
-default = n2
-n2 = winDialogString(message, default)
-n2 = type.convert(n2, na.strings = "NA", as.is = FALSE, dec = ".")
-
-# Input moving average coefficient
-message = "Please specify MA-component"
-default = beta
-beta = winDialogString(message, default)
-beta = type.convert(beta, na.strings = "NA", as.is = FALSE, dec = ".")
+n1   = 50
+n2   = 250
+beta = 0.5
 
 # Simulation of MA(1)-processes
-set.seed(10)
-x1 = arima.sim(n1, model = list(ar = 0, d = 0, ma = beta), rand.gen = function(n1) rnorm(n1, 
+set.seed(2)
+x1 = arima.sim(n1, model = list(ma = beta), rand.gen = function(n1) rnorm(n1, 
     0, 1))
-x2 = arima.sim(n2, model = list(ar = 0, d = 0, ma = beta), rand.gen = function(n2) rnorm(n2, 
+x2 = arima.sim(n2, model = list(ma = beta), rand.gen = function(n2) rnorm(n2, 
     0, 1))
 
 x1 = as.matrix(x1)
